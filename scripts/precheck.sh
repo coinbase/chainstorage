ab83996186c1fe1729f0ac3e1e10b069dee5ef2c
Leo Liang <leozc@ColdPie-2.local>
2022-12-04 22:44:07 -0800

    adding precheck.sh and prevent boostrap to install system package - since it is too hard to get it right across platforms

---
+++ scripts/precheck.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Check if protobuf is installed
+if ! [ -x "$(command -v protoc)" ]; then
+  echo 'Error: protobuf is not installed. Please refer https://grpc.io/docs/protoc-installation/ for installation instruction' >&2
+  exit 1
+fi
+
+# Check if protobuf is installed
+if ! [ -x "$(command -v yq)" ]; then
+  echo 'Error: yq is not installed.' >&2
+  exit 1
+fi
