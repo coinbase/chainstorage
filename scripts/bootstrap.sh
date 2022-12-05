b0fbfb182915c370379378c6feb490ec67abb1c3
Leo Liang <leozc@users.noreply.github.com>
2022-12-04 19:07:54 -0800

    adding precheck.sh and prevent boostrap to install system package - since it is too hard to get it right across platforms

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,12 +2,8 @@
 
 set -exo pipefail
 
-PROTOC_VER=3.13.0
-PROTOC_ZIP=protoc-${PROTOC_VER}-osx-x86_64.zip
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/${PROTOC_ZIP}
-unzip -o ${PROTOC_ZIP} -d /usr/local bin/protoc
-unzip -o ${PROTOC_ZIP} -d /usr/local 'include/*'
-rm -f ${PROTOC_ZIP}
+source ./scripts/precheck.sh
+
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.25
 go install github.com/mikefarah/yq/v4@v4.27.5
@@ -15,6 +11,6 @@ go install github.com/golang/mock/mockgen@v1.6.0
 go install github.com/gordonklaus/ineffassign@v0.0.0-20210914165742-4cc7213b9bc8
 go install github.com/kisielk/errcheck@v1.6.0
 go install github.com/go-bindata/go-bindata/go-bindata@v3.1.2
-go install golang.org/x/tools/cmd/goimports@v0.1.10
+go install git.org/x/tools/cmd/goimports@v0.1.10
 go mod download
 go mod tidy
