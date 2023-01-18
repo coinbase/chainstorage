93ef08c88292cd10e596fbe0c7f32922ffa1cb42
Leo Liang <leozc@leozc.com>
2023-01-17 21:41:37 -0800

    precheck local change

--- scripts/precheck.sh
+++ scripts/precheck.sh
@@ -7,3 +7,4 @@ if ! [ -x "$(command -v protoc)" ]; then
   echo 'Error: protobuf is not installed. Please refer https://grpc.io/docs/protoc-installation/ for installation instruction' >&2
   exit 1
 fi
+
