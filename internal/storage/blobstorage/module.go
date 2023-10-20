88e8bc20196f58e2f78cbcabf888ccb5e91fcbd6
Yuanhai He <i@bestmike007.com>
2023-10-20 10:32:47 +0800

    chore: remove downloader from blob storage module (#41)
    The blob storage is not depending on block downloader.

--- internal/storage/blobstorage/module.go
+++ internal/storage/blobstorage/module.go
@@ -2,11 +2,8 @@ package blobstorage
 
 import (
 	"go.uber.org/fx"
-
-	"github.com/coinbase/chainstorage/internal/storage/blobstorage/downloader"
 )
 
 var Module = fx.Options(
 	fx.Provide(New),
-	downloader.Module,
 )
