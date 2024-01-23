821aa211329a7a4ae90a23da38fe36619af6a761
bestmike007 <i@bestmike007.com>
2024-01-22 21:11:30 -0600

    chore: revert logging
    Signed-off-by: bestmike007 <i@bestmike007.com>

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -715,7 +715,7 @@ func (s *Server) newBlockFile(block *api.BlockMetadata) (*api.BlockFile, error) 
 	compression := storage_utils.GetCompressionType(key)
 	fileUrl, err := s.blobStorage.PreSign(context.Background(), key)
 	if err != nil {
-		s.logger.Error("block file s3 presign error", zap.String("key", key), zap.Error(err))
+		s.logger.Error("block file s3 presign error", zap.Reflect("block", block), zap.Error(err))
 		return nil, status.Errorf(codes.Internal, "internal block file url generation error: %+v", err)
 	}
 
