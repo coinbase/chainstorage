d262ccd5babb47220d34b716cabdfb90a89c24de
Sam Zhao <20300075+samsuse@users.noreply.github.com>
2024-06-18 17:23:57 +0800

    validate ltc script pubkey

--- internal/blockchain/parser/bitcoin/bitcoin_native.go
+++ internal/blockchain/parser/bitcoin/bitcoin_native.go
@@ -193,10 +193,12 @@ func validateBitcoinScriptPubKey(sl validator.StructLevel) {
 			}
 		}
 	// Types that we expect to be able to parse address for
-	case bitcoinScriptTypePubKeyHash, bitcoinScriptTypeScriptHash, bitcoinScriptTypeWitnessV0PubKeyHash, bitcoinScriptTypeWitnessV0ScriptHash, bitcoinScriptTypeWitnessUnknown, bitcoinScriptTypeWitnessV1Taproot, bitcoinScriptTypeMwebPegin, bitcoinScriptTypeMwebHogaddr:
+	case bitcoinScriptTypePubKeyHash, bitcoinScriptTypeScriptHash, bitcoinScriptTypeWitnessV0PubKeyHash, bitcoinScriptTypeWitnessV0ScriptHash, bitcoinScriptTypeWitnessUnknown, bitcoinScriptTypeWitnessV1Taproot:
 		if len(address) == 0 {
 			sl.ReportError(address, "Address[main]", "Address[main]", "bspk_a", "")
-		}
+		} // Types that we expect to be able to parse address for
+	case bitcoinScriptTypeMwebPegin, bitcoinScriptTypeMwebHogaddr:
+		// https://github.com/litecoin-project/litecoin/blob/cd1660afaf5b31a80e797668b12b5b3933844842/src/script/standard.cpp#L60
 	default:
 		sl.ReportError(address, "Address[unsupported]", "Address[unsupported]", "bspk_as", "")
 	}
